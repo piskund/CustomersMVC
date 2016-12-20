68131d544529d91f091bd8115aa03ca01819d6d7
dip <dmytro.piskun@gmail.com>
2016-12-21 01:27:15 +0200

    Select all moved to the pager section.

--- Customers.Web/Scripts/jquery.custom.js
+++ Customers.Web/Scripts/jquery.custom.js
@@ -1,35 +1,39 @@
-﻿function toggleChecked(status) {
-    $("#checkboxes input").each(function () {
-        // Set the checked status of each to match the 
-        // checked status of the check all checkbox:
-        $(this).prop("checked", status);
-    });
+﻿// Change status of all checkboxes marked with class "checkboxes".
+function toggleChecked(status) {
+    $("#checkboxes input")
+        .each(function() {
+            // Set the checked status of each to match the 
+            // checked status of the check all checkbox:
+            $(this).prop("checked", status);
+        });
 }
 
-$(document).ready(function () {
-    // Get a value of the selectAll box:
-    var checkAllBox = $("#selectAll");
-
-    //Set the default value of the global checkbox to false:
-    checkAllBox.prop('checked', false);
-
-    // Attach the call to toggleChecked to the
-    // click event of the global checkbox:
-    checkAllBox.click(function () {
-        var status = checkAllBox.prop('checked');
-        toggleChecked(status);
-    });
-});
-
-$(document).ready(function () {
-    var link = document.getElementById("selectAllLink");
-
-    // Attach selection of all checkboxes to hyperlink click
-    link.onclick = function () {
+// Set/unset checkboxes value base on master checkbox value.
+$(document)
+    .ready(function() {
+        // Get a value of the selectAll box:
         var checkAllBox = $("#selectAll");
-        var status = checkAllBox.prop('checked');
-        checkAllBox.prop("checked", !status);
-        toggleChecked(!status);
-        return false;
-    }
-});
+
+        //Set the default value of the global checkbox to false:
+        checkAllBox.prop("checked", false);
+
+        // Attach the call to toggleChecked to the
+        // click event of the global checkbox:
+        checkAllBox.click(function() {
+            var status = checkAllBox.prop("checked");
+            toggleChecked(status);
+        });
+    });
+
+// Set/unset checkboxes value if "Select All" link clicked.
+$(document)
+    .ready(function() {
+        $(".selectAllLink")
+            .click(function() {
+                var checkAllBox = $("#selectAll");
+                var status = checkAllBox.prop("checked");
+                checkAllBox.prop("checked", !status);
+                toggleChecked(!status);
+                return false;
+            });
+    });
