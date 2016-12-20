e8f74f874f0731cb8366748fabdfd6d5eb9376a3
dip <dmytro.piskun@gmail.com>
2016-12-20 22:59:57 +0200

    Added select all with js and clickable emails.

---
+++ Customers.Web/Scripts/jquery.custom.js
@@ -0,0 +1,35 @@
+﻿function toggleChecked(status) {
+    $("#checkboxes input").each(function () {
+        // Set the checked status of each to match the 
+        // checked status of the check all checkbox:
+        $(this).prop("checked", status);
+    });
+}
+
+$(document).ready(function () {
+    // Get a value of the selectAll box:
+    var checkAllBox = $("#selectAll");
+
+    //Set the default value of the global checkbox to false:
+    checkAllBox.prop('checked', false);
+
+    // Attach the call to toggleChecked to the
+    // click event of the global checkbox:
+    checkAllBox.click(function () {
+        var status = checkAllBox.prop('checked');
+        toggleChecked(status);
+    });
+});
+
+$(document).ready(function () {
+    var link = document.getElementById("selectAllLink");
+
+    // Attach selection of all checkboxes to hyperlink click
+    link.onclick = function () {
+        var checkAllBox = $("#selectAll");
+        var status = checkAllBox.prop('checked');
+        checkAllBox.prop("checked", !status);
+        toggleChecked(!status);
+        return false;
+    }
+});
