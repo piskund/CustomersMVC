82f8542526b53e323b7373ca808b459f66bcdcca
dip <dmytro.piskun@gmail.com>
2016-12-28 23:36:59 +0200

    Tabs updated.

---
+++ Customers.Web/Helpers/HtmlHelpers.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace Customers.Web.Helpers
+{
+    public static class HtmlHelpers
+    {
+        public static string IsActive(this HtmlHelper htmlHelper, string controller, string action)
+        {
+            var routeData = htmlHelper.ViewContext.RouteData;
+
+            var routeAction = routeData.Values["action"].ToString();
+            var routeController = routeData.Values["controller"].ToString();
+
+            var returnActive = (controller == routeController) && (action == routeAction);
+
+            return returnActive ? "active" : "";
+        }
+    }
+}
